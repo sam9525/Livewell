6b9340ab3bcc306a57961deb37f1c224a3e3caa8
Sam Huang <sheng-hsiang.huang@student.adelaide.edu.au>
2025-08-08 00:43:53 +0930

    Develop on Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'shared/shared.dart';
 import 'views/signin.dart';
 import 'views/signup.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
