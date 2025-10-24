c97712fd8a6fe54f8056c2839d640f6d2dfc75a6
Sam Huang <sheng-hsiang.huang@student.adelaide.edu.au>
2025-10-24 23:55:13 +1030

    Optimize page margin

--- lib/views/goal_page.dart
+++ lib/views/goal_page.dart
@@ -7,55 +7,59 @@ class Goal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            // AI Recommendations Section
-            RecommendationsList(),
-            SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.25),
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              children: [
+                // AI Recommendations Section
+                RecommendationsList(),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.25),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: NumberPicker(
-                title: "Daily water intake (ml)",
-                begin: 500,
-                end: 4000,
-                jump: 50,
-              ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.25),
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
+                  child: NumberPicker(
+                    title: "Daily water intake (ml)",
+                    begin: 500,
+                    end: 4000,
+                    jump: 50,
                   ),
-                ],
-              ),
-              child: NumberPicker(
-                title: "Daily steps",
-                begin: 2000,
-                end: 120000,
-                jump: 100,
-                isWaterIntake: false,
-              ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.25),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: NumberPicker(
+                    title: "Daily steps",
+                    begin: 2000,
+                    end: 120000,
+                    jump: 100,
+                    isWaterIntake: false,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
